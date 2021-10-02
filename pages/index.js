7bdd54d8469b510dbb862475fe50e8f120d1ab66
Beata Tomczak <tma.btty@gmail.com>
2021-10-02 23:36:11 +0200

    Set up next.js & tailwind css

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Airbnb</title>
+        <link rel='icon' href='/airbnb.ico' />
+      </Head>
+
+      <h1>Airbnb</h1>
+    </div>
+  )
+}
