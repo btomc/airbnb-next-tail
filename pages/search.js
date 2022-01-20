e2ead2317557d98aea76c1af23a78200dd2de873
Beata Tomczak <tma.btty@gmail.com>
2022-01-20 22:18:53 +0100

    Build the Search Page

---
+++ pages/search.js
@@ -0,0 +1,38 @@
+import { useRouter } from 'next/dist/client/router'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import { format } from 'date-fns'
+
+function Search() {
+  const router = useRouter()
+  const { location, startDate, endDate, noOfGuests } = router.query
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+  // console.log(router.query)
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+
+      <main className='flex'>
+        <section className='flex-grow pt-14 px-6'>
+          <p className='text-xs'>300+ Stays - {range} - for {noOfGuests} guests</p>
+
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+            <p className='btn'>Cancellation Flexibility</p>
+            <p className='btn'>Type of Place</p>
+            <p className='btn'>Price</p>
+            <p className='btn'>Rooms and Beds</p>
+            <p className='btn'>More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Search
